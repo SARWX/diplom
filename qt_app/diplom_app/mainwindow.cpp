ba5064398034a9095c57860f43577e31f49b3c34
Timofei <tictacnumt@gmail.com>
2024-03-04 00:52:46 +0300

    some markdown afforts, buttons aren't stratch propperly

--- qt_app/diplom_app/mainwindow.cpp
+++ qt_app/diplom_app/mainwindow.cpp
@@ -1,4 +1,8 @@
 #include "mainwindow.h"
+#include <QPushButton>
+#include <QSizePolicy>
+#include <QLineEdit>
+#include <QLabel>
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -46,14 +50,40 @@ MainWindow::MainWindow(QWidget *parent)
     scene4->setBackgroundBrush(Qt::cyan);
     graphicsView4->setScene(scene4);
 
+    // Создание боковых элементов управления
+    QPushButton *button1 = new QPushButton("Нажми\nменя", centralWidget);
+    QPushButton *button2 = new QPushButton("Нажми\nменя", centralWidget);
+    QPushButton *button3 = new QPushButton("Нажми\nменя", centralWidget);
+    QPushButton *button4 = new QPushButton("Нажми\nменя", centralWidget);
+    QPushButton *button5 = new QPushButton("Нажми\nменя", centralWidget);
+    QPushButton *button6 = new QPushButton("Нажми\nменя", centralWidget);
+
+    // Командная строка
+    QLineEdit *lineEdit = new QLineEdit(centralWidget);
+    // Текстовое поле вывода
+    QLabel *output_line = new QLabel("Устройство не подключено", centralWidget);
+    output_line->setStyleSheet("background-color: lightblue;");
+
 
     // Создайте компоновщик сетки
     QGridLayout *gridLayout = new QGridLayout(centralWidget);
 
     // Добавьте QGraphicsView в сетку
-    gridLayout->addWidget(graphicsView1, 0, 0);  // Первая строка, первый столбец
-    gridLayout->addWidget(graphicsView2, 1, 0);  // Вторая строка, первый столбец
-    gridLayout->addWidget(graphicsView3, 0, 1);  // Первая строка, второй столбец
-    gridLayout->addWidget(graphicsView4, 1, 1);  // Вторая строка, второй столбец
-
+    gridLayout->addWidget(graphicsView1, 0, 1, 3, 1);  // Первая строка, первый столбец
+    gridLayout->addWidget(graphicsView2, 3, 1, 3, 1);  // Вторая строка, первый столбец
+    gridLayout->addWidget(graphicsView3, 0, 2, 3, 1);  // Первая строка, второй столбец
+    gridLayout->addWidget(graphicsView4, 3, 2, 3, 1);  // Вторая строка, второй столбец
+    gridLayout->addWidget(button1, 0, 0, 1, 1);  // Третья строка, первый и второй столбец
+    gridLayout->addWidget(button2, 1, 0, 1, 1);  // Третья строка, первый и второй столбец
+    gridLayout->addWidget(button3, 2, 0, 1, 1);  // Третья строка, первый и второй столбец
+    gridLayout->addWidget(button4, 3, 0, 1, 1);  // Третья строка, первый и второй столбец
+    gridLayout->addWidget(button5, 4, 0, 1, 1);  // Третья строка, первый и второй столбец
+    gridLayout->addWidget(button6, 5, 0, 1, 1);  // Третья строка, первый и второй столбец
+    gridLayout->addWidget(output_line, 6, 1, 1, 2);  // Третья строка, первый и второй столбец
+    gridLayout->addWidget(lineEdit, 7, 1, 1, 2);  // Третья строка, первый и второй столбец
+    // Установите растяжение всех строк
+    int numRows = gridLayout->rowCount();
+    for (int row = 0; row < numRows; row++) {
+        gridLayout->setRowStretch(row, 1);
+    }
 }
