6d10c5ee2dbe24bb85ca7628e1417a3b37b61aef
Timofei <tictacnumt@gmail.com>
2024-03-17 22:40:42 +0300

    nothing

--- qt_app/diplom_app/trilaterator.h
+++ qt_app/diplom_app/trilaterator.h
@@ -9,14 +9,19 @@
 #define POS_Y 1.0           // Задает координату якоря Y
 #define POS_Z 1.0           // Задает координату якоря Z
 
+#define SPEED_DELAY 0.1     // Задержка в измененении скорости
+
 class Trilaterator : public QObject
 {
     Q_OBJECT
 
 private:
-    coordinates cur_point;
-    int iterator;
-    AveragingArray avg_array{};
+    coordinates cur_point;          // текущие координаты точки
+    coordinates prev_point;         // координаты предыдущей точки
+    AveragingArray avg_array{};     // специальный объект для усреднения измеренийы
+    coordinates cur_dr;             // текущее перемещение
+    double cur_speed;   // текущий вектор скорости
+
 
 public:
     Trilaterator() : cur_point{0.0, 0.0, 0.0}
@@ -27,6 +32,7 @@ public:
     // Метод трилатерации
     coordinates trilaterate(double r1, double r2, double r3)
     {
+        prev_point = cur_point;     // Запишем координаты предыдущей точки
         cur_point.y = ((pow(r1, 2) - pow(r2, 2) + pow(POS_Y, 2)) / (2*POS_Y));
         cur_point.z = ((pow(r1, 2) - pow(r3, 2) + pow(POS_Z, 2)) / (2*POS_Z));
         cur_point.x = sqrt(abs(pow(r1, 2) - pow(cur_point.y, 2) - pow(cur_point.z, 2)));
@@ -36,7 +42,8 @@ public:
         avg_array.AddPredictVector(cur_point);
 //        delta += avg_array.GetAvg();
         // возвращаем усредненное значение
-        return (avg_array.GetAvg() /*+ avg_array.GetPredictVector()*/);
+        cur_point = avg_array.GetAvg();
+        return (cur_point);
     };
 
 signals:
@@ -45,6 +52,18 @@ signals:
 public slots:
     // Слот, который генерирует числа и отправляет сигнал
     void convertDistance(double r1, double r2, double r3, int point_nimber = 0) {
+        trilaterate(r1, r2, r3);                        // производим трилатерацию с усреднением
+        coordinates dr = cur_point - prev_point;        // получаем перемещение dr
+        coordinates dr_real = dr                        // сохраним реальное dr
+        dr = project_vector(cur_dr, dr);                // спроецировать вектор dr на cur_dr
+        normalize_vector(&dr);                          // нормируем вектор
+        dr = cur_speed * dr;                            // зададим итоговое перемещение
+        double speed_dif = to_dist(dr_real - dr);       // длина ветора С УЧЕТОМ ЗНАКА + не успевает - опережает
+        speed += SPEED_DELAY * speed_dif;
+
+
+
+        coordinates speed_vector = get_speed_vecor();
         emit coordinateChanged(trilaterate(r1, r2, r3), point_nimber);
     }
 
